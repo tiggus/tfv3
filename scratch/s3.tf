a8fc110c9ed3ba9ed5af7b05a6080c2fbced17da
tiggus <steve@tiggus.com>
2025-06-20 09:25:45 +0100

    first push

--- scratch/s3.tf
+++
@@ -1,9 +0,0 @@
-# module "website_s3_bucket" {
-#   source      = "./modules/aws-s3"
-#   bucket_name = "39393939393-bucket-39"
-
-#   tags = {
-#     terraform   = "true"
-#     environment = "dev"
-#   }
-# }
